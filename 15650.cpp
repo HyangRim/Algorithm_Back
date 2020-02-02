0f8bade4ee2f019f2d8196f178cc50e1a7d45df9
HyangRim <eyiuta1@gmail.com>
2020-02-03 02:04:29 +0900

    new file

---
+++ 15650.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+int N, M;
+int arr[9];
+bool visited[9];
+void BFS(int cnt) {
+	if (cnt == M) {
+		for (int x = 0; x < M; x++) {
+			cout << arr[x] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int x = 1; x <= N; x++) {
+		if (!visited[x]) {
+			if (cnt == 0 || (cnt > 0 && x > arr[cnt - 1])) {
+				visited[x] = true;
+				arr[cnt] = x;
+				BFS(cnt + 1);
+				visited[x] = false;
+			}
+		}
+	}
+
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> N >> M;
+
+	BFS(0);
+}
+
